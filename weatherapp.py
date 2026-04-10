7d3fabe43afd3406527884164e5e9ce0d902c60a
Usashi <biswasushu99@gmail.com>
2026-04-10 16:43:40 +0530

    Add files via upload

---
+++ weatherapp.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests
+
+st.title("🌦 Weather App (Live API)")
+
+api_key = st.secrets["API_KEY"]   # paste your API key here
+
+city = st.text_input("Enter City Name:")
+
+if st.button("Get Weather"):
+    if city == "":
+        st.warning("Please enter a city name!")
+    else:
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            temp = data["main"]["temp"]
+            humidity = data["main"]["humidity"]
+            condition = data["weather"][0]["description"]
+
+            st.success(f"Weather in {city}")
+            st.write("🌡 Temperature:", temp, "°C")
+            st.write("💧 Humidity:", humidity, "%")
+            st.write("🌥 Condition:", condition)
+
+        else:
+            st.error("City not found! Please enter a valid city name.")
